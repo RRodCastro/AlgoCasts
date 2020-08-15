4e3c44be73993102cdd4cc5f83647a912dc987ee
Rodrigo <mrcastro9574@gmail.com>
2020-08-15 10:30:10 -0600

    Add event class

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,37 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  
+  constructor() {
+    this.eventNames = {}
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    const event = this.eventNames[eventName]
+    if (!event){
+      this.eventNames[eventName] = [ callback ]
+    } else {
+      this.eventNames[eventName].push(callback)
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if ( this.eventNames[eventName] ){
+      this.eventNames[eventName].forEach((event) => event());
+    }
+    
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    if ( this.eventNames[eventName] ){
+      this.eventNames[eventName] = []
+    }
+    
+  }
 }
 
 module.exports = Events;
